8521af3c98efc006edf271057528be78c4ab7258
César Oyarzo Muñoz <84143087+tinguinito@users.noreply.github.com>
2021-06-16 12:16:06 -0400

    authentication jwt

--- README.md
+++ README.md
@@ -4,5 +4,24 @@
 #Steps
 * npm install
 * node index.js
+* install Postman application
+
+
+#Authorization
+
+1.- use /api/authenticate endpoint and get token with username and password valid
+
+Postman figure
+![img_1.png](img_1.png)
+
+2.- Copy token from body response
+
+3.- Create a new Tab for get list from /api/products
+
+4.- Go authorization tab, choose type Bearer and paste token on input
+![img_2.png](img_2.png)
+5.- click Send! enjoy
+
+
 
 
--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv');
 const app = express()
 const port = 3001
+
+
+
+// get config vars
+dotenv.config();
+
+// access config and get secret key
+const secrect_key = process.env.TOKEN_SECRET;
+
+
+function generateAccessToken(username) {
+    return jwt.sign(username, secrect_key, { expiresIn: '1800s' });
+}
+
+
+
 
 let products = [
     { sku: "prod-1", product: "Manzana verde", quantity: 50, price: 500, un: 'KG', isChecked: false },
     { sku: "prod-2", product: "Manzana Roja", quantity: 40, price: 600, un: 'KG', isChecked: false },
     { sku: "prod-3", product: "Uva Moscatel", quantity: 20, price: 1500, un: 'KG', isChecked: false },
     { sku: "prod-4", product: "Zapallo", quantity: 10, price: 2000, un: 'UN', isChecked: false },
+]
+
+let users = [
+    { 'username': 'caom', 'password': 'qwerty' },
+    { 'username': 'ipchile', 'password': 'ipchile' },
+    { 'username': 'desarrolloweb', 'password': 'winners' },
 ]
 
 
@@ -18,22 +42,64 @@ app.use(cors())
 //espress.json() for get body from request in json format
 app.use(express.json());
 
+
+app.post('/api/authenticate', (req, res) => {
+    console.log('req.body', req.body)
+
+    if (req.body !== undefined && req.body.username !== undefined && req.body.password !== undefined) {
+        const username = req.body.username
+        const pwd = req.body.password
+        console.log('body:', username, pwd)
+
+        const userFiltered = users.filter((user) => user.username === username && user.password === pwd)
+        console.log('userFiltered', userFiltered)
+        if (userFiltered.length > 0 && userFiltered !== undefined) {
+
+            const token = generateAccessToken({ username: userFiltered.username });
+            console.log('token', token)
+            return res.status(200).json(token);
+
+        }
+    }
+    else {
+        return res.sendStatus(403)
+    }
+});
+
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, secrect_key, (err, user) => {
+        console.error("Error:", err)
+
+        if (err) { return res.sendStatus(403) }
+
+        req.user = user
+
+        next()
+    })
+}
+
 app.get('/', (req, res) => {
     res.send('Hello to Api Fruit Store!')
 })
 
 //get List
-app.get('/api/products', (req, res) => {
+app.get('/api/products', authenticateToken, (req, res) => {
     // setTimeout(() => {
     //     console.log(`Esperando 3 segundos`)
     //     return res.send(products)
     // }, 3000);
-    console.log(`/api/products`)
+    console.log(`GET /api/products`)
     return res.status(200).send(products)
 })
 
 //Edit Product
-app.put('/api/products', (req, res) => {
+app.put('/api/products', authenticateToken, (req, res) => {
     console.log(req.body);
     const product = req.body;
 
@@ -53,7 +119,7 @@ app.put('/api/products', (req, res) => {
 })
 
 //Add product
-app.post('/api/products', (req, res) => {
+app.post('/api/products', authenticateToken, (req, res) => {
     console.log(req.body);
     const product = req.body;
 
@@ -70,7 +136,7 @@ app.post('/api/products', (req, res) => {
 })
 
 //delete product
-app.delete('/api/products/:id', (req, res) => {
+app.delete('/api/products/:id', authenticateToken, (req, res) => {
     console.log(req.params.id);
     const sku = req.params.id;
 
